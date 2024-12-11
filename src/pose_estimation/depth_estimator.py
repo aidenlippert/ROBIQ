4611135434d0f55c4806a7cb3db28ad953d26c3e
aidenlippert <aidenlippert@gmail.com>
2024-12-11 01:03:42 -0800

    Update depth_estimator.py

--- src/pose_estimation/depth_estimator.py
+++ src/pose_estimation/depth_estimator.py
@@ -5,7 +5,8 @@ from torchvision.transforms import Compose, ToTensor, Resize
 
 class DepthEstimator:
     def __init__(self, model_type='DPT_Large'):
-        self.model = torch.hub.load("intel-isl/MiDaS", model_type)
+        # Use MiDaS v3.1 (best version for monocular depth estimation)
+        self.model = torch.hub.load("intel-isl/MiDaS", "MiDaS_v3.1")
         self.model.eval()
         self.transforms = Compose([
             Resize((384, 384)),
