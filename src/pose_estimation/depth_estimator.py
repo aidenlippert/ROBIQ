96ff37f9ed61a28c0269157563e60eac669006e9
aidenlippert <aidenlippert@gmail.com>
2024-12-11 01:21:03 -0800

    Update depth_estimator.py

--- src/pose_estimation/depth_estimator.py
+++ src/pose_estimation/depth_estimator.py
@@ -1,12 +1,12 @@
-import cv2
 import torch
+import cv2
 import numpy as np
 from torchvision.transforms import Compose, ToTensor, Resize
 
 class DepthEstimator:
     def __init__(self, model_type='DPT_Large'):
-        # Use MiDaS v3.1 (best version for monocular depth estimation)
-        self.model = torch.hub.load("intel-isl/MiDaS", "MiDaS_v3.1")
+        # Load the MiDaS model for depth estimation
+        self.model = torch.hub.load("intel-isl/MiDaS", model_type)
         self.model.eval()
         self.transforms = Compose([
             Resize((384, 384)),
@@ -15,6 +15,7 @@ class DepthEstimator:
 
     def estimate_depth(self, frame):
         """
+        Estimate depth from a single image frame.
         Input:
             - frame: BGR image from OpenCV
         Returns:
@@ -25,3 +26,14 @@ class DepthEstimator:
         with torch.no_grad():
             depth_map = self.model(input_tensor).squeeze().numpy()
         return depth_map
+
+    def get_depth_at_point(self, frame, x, y):
+        """
+        Given a 2D point (x, y) from the pose landmarks, return the corresponding depth value.
+        """
+        depth_map = self.estimate_depth(frame)
+        height, width = depth_map.shape
+        # Normalize the coordinates
+        x = int(x * width)
+        y = int(y * height)
+        return depth_map[y, x]
